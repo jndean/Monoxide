393c9bec1f2c8f111078d887b92c699b4d1a3b29
jndean <josefdean@blueyonder.co.uk>
2020-02-01 18:42:17 +0000

    Figuring out how variable will be stored and moved around on the stack

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,34 @@
+#![allow(dead_code)]
+
 use std::fs;
+use std::fmt;
 
 
+#[derive(Debug)]
 enum Variable {
-    Frac(i32),
-    Array(i32),
-    Str(i32)
+    Frac(Box<i32>),
+    Array(Box<Vec<Variable>>)
 }
 
-struct VarArray {
-    memory: &Vec<Variable>
+impl fmt::Display for Variable{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Variable::Frac(valbox) => write!(f, "Frac({})", *valbox),
+            Variable::Array(_) => write!(f, "Array(...)")
+        }
+    }
 }
 
 enum Instruction {
     LoadConst{idx: u16},
     LoadLocal{idx: u16},
+    SetLocal{val: i32},
     StoreLocal{idx: u16}
 }
 
+#[derive(Debug)]
 enum StackObject {
-    Variable(Variable),
-    Other
+    Variable(Variable)
 }
 
 
@@ -38,8 +47,9 @@ impl Scope {
         while self.ip < self.code.len() {
             match self.code[self.ip] {
                 Instruction::LoadConst{idx} => self.load_const(idx),
-                Instruction::LoadLocal{idx} => println!("LoadLocal({})", idx),
-                Instruction::StoreLocal{idx} => println!("StoreLocal({})", idx),
+                Instruction::LoadLocal{idx} => self.load_local(idx),
+                Instruction::StoreLocal{idx} => self.store_local(idx),
+                _ => println!("BLANK()"),
 
             }
             self.ip += 1;
@@ -48,16 +58,38 @@ impl Scope {
 
         
     fn load_const(&mut self, idx: u16) {
-        let var = &self.consts[idx as usize];
+        match &self.consts[idx as usize] {
+            Variable::Frac(valbox) => {
+                self.stack.push(
+                    StackObject::Variable(
+                        Variable::Frac(
+                            Box::new(**valbox)
+                        )
+                    )
+                );
+            }
+            Variable::Array(_) => println!("ConstLoading array")
+        }
+    }  
+      
+    fn load_local(&mut self, idx: u16) {
+        let var = &self.locals[idx as usize];
         match var {
-            Variable::Frac(val) => println!("ConstLoading {}", val),
-            Variable::Array(val) => println!("ConstLoading {}", val),
-            Variable::Str(val) => println!("ConstLoading {}", val),
+            Variable::Frac(valbox) => println!("LocalLoading {}", *valbox),
+            Variable::Array(_) => println!("LocalLoading array")
+        }
+    }
+
+    fn store_local(&mut self, idx: u16) {
+        let obj = self.stack.pop().expect("Popped off empty stack");
+        match obj {
+            StackObject::Variable(var) => {
+                self.locals[idx as usize] = var;
+            },
+            _ => panic!("Excpected to pop a variable off the stack, found something else!")
         }
     }
 }
-
-
 
 
 struct Token {
@@ -79,20 +111,21 @@ fn tokenise(data: &String) -> Vec<Token> {
     Vec::new()
 }
 
+
 fn main() {
     let code = vec![
-        Instruction::LoadLocal {idx: 0},
-        Instruction::StoreLocal {idx: 1},
         Instruction::LoadConst {idx: 0},
+        Instruction::StoreLocal {idx: 1}
     ];
 
-    let mut locals = vec![
-        Variable::Frac(1701),
-        Variable::Array(99),
+    let locals = vec![
+        Variable::Frac(Box::new(0)),
+        Variable::Frac(Box::new(1)),
+        Variable::Frac(Box::new(2))
     ];
 
     let consts = vec![
-        Variable::Frac(101)
+        Variable::Frac(Box::new(99))
     ];
 
     let mut scope = Scope{
@@ -103,11 +136,13 @@ fn main() {
         consts
     };
     scope.run();
+
+    println!("Stack[0] = {:?}", scope.locals);
     
-    let src = fs::read_to_string("src/main.rs")
+    /*let src = fs::read_to_string("src/main.rs")
         .expect("File io error");
 
-    //let tokens = tokenise(&src);
+    let tokens = tokenise(&src);*/
 
 
 }
