2e55a406306d992d0db41dc8f3f290264274c4ee
jndean <josefdean@blueyonder.co.uk>
2020-02-07 22:39:08 +0000

    Add keyword recognition to tokeniser

--- src/tokeniser.rs
+++ src/tokeniser.rs
@@ -1,5 +1,7 @@
 
+use std::collections::HashSet;
 use regex;
+
 
 #[derive(Debug)]
 pub struct Token {
@@ -12,25 +14,67 @@ pub struct Token {
 
 pub fn tokenise(data: &String) -> Vec<Token> {
 
-    let name_regex = regex::Regex::new(r"^[a-zA-Z_][a-zA-Z_0-9\.]+").unwrap();
+    let name_regex = regex::Regex::new(r"^[a-zA-Z_][a-zA-Z_0-9\.]*").unwrap();
     let return_regex = regex::Regex::new(r"^\n").unwrap();
-    let ignore_regex = regex::Regex::new(r"([$][^$]*[$])|([ \t\r\f\v]+)").unwrap();
+    let ignore_regex = regex::Regex::new(r"^([$][^$]*[$])|([ \t\r\f\v]+)").unwrap();
+    let number_regex = regex::Regex::new(r"^\d+(/\d+)?").unwrap();
+    let mut keywords = HashSet::new();
+    keywords.insert("import");
+    keywords.insert("as");
+    keywords.insert("global");
+    keywords.insert("let");
+    keywords.insert("unlet");
+    keywords.insert("func");
+    keywords.insert("return");
+    keywords.insert("println");
+    keywords.insert("print");
+    keywords.insert("if");
+    keywords.insert("fi");
+    keywords.insert("else");
+    keywords.insert("loop");
+    keywords.insert("pool");
+    keywords.insert("for");
+    keywords.insert("rof");
+    keywords.insert("call");
+    keywords.insert("uncall");
+    keywords.insert("do");
+    keywords.insert("undo");
+    keywords.insert("yield");
+    keywords.insert("swap");
+    keywords.insert("push");
+    keywords.insert("pop");
+    keywords.insert("try");
+    keywords.insert("catch");
+    keywords.insert("yrt");
+    keywords.insert("promote");
+    keywords.insert("in");
+    keywords.insert("to");
+    keywords.insert("by");
+    keywords.insert("tensor");
+    keywords.insert("barrier");
+    keywords.insert("mutex");
+    keywords.insert("xetum");
+    keywords.insert("TID");
 
     let mut ret = Vec::new();
     let mut pos = 0;
     let mut line = 1;
     let mut col = 0;
+    let mut skip_newline = true;
     while pos < data.len() {
         match return_regex.find(&data[pos..]) {
             Some(m) => {
-                ret.push(Token{
-                    type_: String::from("NEWLINE"), 
-                    string_: String::from(&data[pos .. pos + m.end()]),
-                    line, col
-                });
+                if !skip_newline {
+                    ret.push(Token{
+                        type_: String::from("NEWLINE"), 
+                        string_: String::from(&data[pos .. pos + m.end()]),
+                        line, col
+                    });
+                }
                 pos += m.end();
                 line += 1;
                 col = 0;
+                skip_newline = true;
                 continue;
             }
             None => ()
@@ -38,36 +82,55 @@ pub fn tokenise(data: &String) -> Vec<Token> {
         
         match name_regex.find(&data[pos..]) {
             Some(m) => {
+                let string_ = &data[pos .. pos + m.end()];
+                let type_ = if keywords.contains(string_) {
+                    string_
+                } else {
+                    "NAME"
+                };
                 ret.push(Token{
-                    type_: String::from("NAME"), 
+                    type_: String::from(type_), 
+                    string_: String::from(string_),
+                    line, col
+                });
+                skip_newline = false;
+                pos += m.end();
+                col += m.end();
+                continue;
+            }
+            None => ()
+        };
+        
+        match number_regex.find(&data[pos..]) {
+            Some(m) => {
+                ret.push(Token{
+                    type_: String::from("NUMBER"), 
                     string_: String::from(&data[pos .. pos + m.end()]),
                     line, col
                 });
                 pos += m.end();
                 col += m.end();
+                skip_newline = false;
                 continue;
             }
-            None => {
-                println!("{}: No match", pos);
-                pos += 1;
-            }
+            None => ()
         };
 
         match ignore_regex.find(&data[pos..]) {
             Some(m) => {
                 pos += m.end();
-                let newlines = data[m.start()..m.end()].matches("\n").count();
-                line += newlines;
-                if newlines > 0 {
-                    //data[m.start()..m.end()].match_indices(r"\n").count()
+                let mut matches: Vec<(usize, &str)> = 
+                    data[m.start()..m.end()].match_indices(r"\n").collect();
+                line += matches.len();
+                if matches.len() > 0 {
+                    let lastpos = matches.pop().unwrap().0;
+                    col = m.end() - lastpos;
+                } else {
+                    col += m.end();
                 }
-                col += m.end();
                 continue;
             }
-            None => {
-                println!("{}: No match", pos);
-                pos += 1;
-            }
+            None => ()
         };
     }
 
