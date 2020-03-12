08dfb9438c8d1079fe271c03608472746998c383
jndean <josefdean@blueyonder.co.uk>
2020-03-12 23:43:33 +0000

    Experiment moving parser to Macro

--- src/parser.rs
+++ src/parser.rs
@@ -6,7 +6,8 @@ use crate::tokeniser::Token;
 use crate::parsetree::{
     StatementNode, ExpressionNode, LookupNode, LetUnletNode,
     FractionNode, BinopNode, IfNode, ModopNode, FunctionNode,
-    CatchNode, ArrayLiteralNode, Module, RefUnrefNode
+    CatchNode, ArrayLiteralNode, Module, RefUnrefNode, CallUncallNode,
+    CallChainNode
 };
 use crate::interpreter::{Fraction, Instruction};
 
@@ -29,9 +30,68 @@ pub enum Parsed {
     LookupNode(Option<LookupNode>),
     IfNode(Option<IfNode>),
     CatchNode(Option<CatchNode>),
+    CallUncallNode(Option<CallUncallNode>),
+    CallChainNode(Option<CallChainNode>),
     FunctionNode(Option<FunctionNode>),
     ArrayLiteralNode(Option<ArrayLiteralNode>)
 }
+
+
+#[allow(unused_macros)]
+macro_rules! rule {
+    ($func_name:ident -> $ret_type:ident $body:block) => {
+        fn $func_name(&mut self) -> Option<$ret_type> {
+            fn raw_func(parser: &mut Parser) -> Option<$ret_type> $body;
+            let pos = self.mark();
+            let key = (pos, String::from(stringify!($func_name)));
+            match self.memo.get(&key) {
+                Some((end, result)) => {
+                    let end = *end;
+                    let result = (*result).clone();
+                    if let Parsed::$ret_type(ret) = result {
+                        self.reset(end);
+                        return ret;
+                    } else {unreachable!()}
+                },
+                None => {
+                    let result = raw_func(self);
+                    let new_pos = self.mark();
+                    let memo = Parsed::$ret_type(result.clone());
+                    self.memo.insert(key, (new_pos, memo));
+                    result
+                }
+            }
+        }
+    }
+}
+
+
+macro_rules! parse {
+    ($sel:ident ; $($tail:tt)*) => {
+        let pos = $sel.mark();
+        parse_recurse!($sel; $($tail)*);
+        $sel.reset(pos);
+    };
+}
+
+macro_rules! parse_recurse {
+    ($sel:ident ; $res_name:ident : $rule_expr:expr, $($tail:tt)*) => {
+        if let Some($res_name) = $rule_expr {
+	        parse_recurse!($sel; $($tail)*);
+        }
+    };
+    ($sel:ident ; ? $res_name:ident : $rule_expr:expr , $($tail:tt)*) => {
+        let $res_name = $rule_expr;
+        parse_recurse!($sel; $($tail)*);
+    };
+    ($sel:ident ; $string:tt, $($tail:tt)*) => {
+        if $sel.expect_literal($string) {
+	        parse_recurse!($sel; $($tail)*);
+	    }
+    };
+    ($sel:ident ; $body:block) => {$body};
+}
+
 
 macro_rules! memoise {
     ($raw_func:ident as $out_func:ident -> $ret_type:ident) => {
@@ -59,7 +119,6 @@ macro_rules! memoise {
     }
 }
 
-#[allow(unused_macros)]
 macro_rules! memoise_recursive {
     ($raw_func:ident as $out_func:ident -> $ret_type:ident) => {
         fn $out_func(&mut self) -> Option<$ret_type> {
@@ -218,8 +277,45 @@ impl Parser {
         if let Some(stmt) = self.modop_stmt() {return Some(stmt);}
         if let Some(stmt) = self.if_stmt() {return Some(stmt);}
         if let Some(stmt) = self.catch_stmt() {return Some(stmt);}
+        if let Some(stmt) = self.call_stmt() {return Some(stmt);}
         None
     }
+
+
+    memoise!(call_stmt_ as call_stmt -> StatementNode);
+    pub fn call_stmt_(&mut self) -> Option<StatementNode> {
+        let pos = self.mark();
+
+        if let Some(calluncall) = self.calluncall_block() {
+            return Some(StatementNode::CallChainNode(Box::new(
+                CallChainNode{
+                    calls: vec![calluncall],
+                    stolen_args: Vec::new(),
+                    return_args: Vec::new(),
+                }
+            )))
+        }
+
+        self.reset(pos);
+        None
+    }
+
+    memoise!(calluncall_block_ as calluncall_block -> CallUncallNode);
+    pub fn calluncall_block_(&mut self) -> Option<CallUncallNode> {
+        let pos = self.mark();
+
+        let is_uncall = self.expect_literal("~");
+        if let Some(name) = self.expect_type("NAME") {
+        if self.expect_literal("(") {
+        let borrow_args = self.join(Parser::lookup, ",");
+        if self.expect_literal(")") {
+            return Some(CallUncallNode{is_uncall, borrow_args, name: name.string_})
+        }}};
+
+        self.reset(pos);
+        None
+    }
+
 
     memoise!(catch_stmt_ as catch_stmt -> StatementNode);
     pub fn catch_stmt_(&mut self) -> Option<StatementNode> {
@@ -230,7 +326,7 @@ impl Parser {
         if let Some(expr) = self.expression() {
         if self.expect_literal(")") {
         if self.expect_literal(";") {
-            return Some(StatementNode::Catch(Box::new(
+            return Some(StatementNode::CatchNode(Box::new(
                 CatchNode{expr}
             )));
         }}}}};
@@ -241,6 +337,38 @@ impl Parser {
 
     memoise!(if_stmt_ as if_stmt -> StatementNode);
     pub fn if_stmt_(&mut self) -> Option<StatementNode> {
+
+        parse!(self;
+            "if",
+            "(",
+            fwd_expr : self.expression(),
+            ")",
+            "{",
+            if_stmts : self.repeat(Parser::statement, true),
+            "}",
+            ? else_stmts : self.else_block(),
+            "~",
+            "if",
+            "(",
+            ? bkwd_expr : self.expression(),
+            ")",
+            ";",
+            {
+                let else_stmts = match else_stmts {
+                    Some(stmts) => stmts,
+                    None => Vec::new()
+                };
+                let bkwd_expr = match bkwd_expr {
+                    Some(expr) => expr,
+                    None => fwd_expr.clone()
+                };
+                return Some(StatementNode::IfNode(Box::new(
+                    IfNode{fwd_expr, if_stmts, else_stmts, bkwd_expr}
+                )));
+
+            }
+
+        );
         let pos = self.mark();
 
         if self.expect_literal("if") {
@@ -265,7 +393,7 @@ impl Parser {
                 Some(expr) => expr,
                 None => fwd_expr.clone()
             };
-            return Some(StatementNode::If(Box::new(
+            return Some(StatementNode::IfNode(Box::new(
                 IfNode{fwd_expr, if_stmts, else_stmts, bkwd_expr}
             )));
         }}}}}}}}}}}};
@@ -298,7 +426,7 @@ impl Parser {
         if let Some(op) = self.modop() {
         if let Some(rhs) = self.expression() {
         if self.expect_literal(";") {
-            return Some(StatementNode::Modop(Box::new(
+            return Some(StatementNode::ModopNode(Box::new(
                 ModopNode{lookup, op, rhs}
             )));
         }}}};
@@ -316,7 +444,7 @@ impl Parser {
         if self.expect_literal("&") {
         if let Some(rhs) = self.lookup() {
         if self.expect_literal(";") {
-            return Some(StatementNode::RefUnref(Box::new(
+            return Some(StatementNode::RefUnrefNode(Box::new(
                 RefUnrefNode{name, rhs, is_unref: false}
             )));
         }}}}};
@@ -327,7 +455,7 @@ impl Parser {
         if self.expect_literal("&") {
         if let Some(rhs) = self.lookup() {
         if self.expect_literal(";") {
-            return Some(StatementNode::RefUnref(Box::new(
+            return Some(StatementNode::RefUnrefNode(Box::new(
                 RefUnrefNode{name, rhs, is_unref: true}
             )));
         }}}}};
@@ -337,15 +465,41 @@ impl Parser {
     }
 
 
+    // rule!(letunlet_stmt -> StatementNode {
     memoise!(letunlet_stmt_ as letunlet_stmt -> StatementNode);
     pub fn letunlet_stmt_(&mut self) -> Option<StatementNode> {
-        let pos = self.mark();
+        
+        parse! (self;
+            name : self.name(),
+            ":=",
+            rhs : self.expression(),
+            ";",
+            {
+                return Some(StatementNode::LetUnletNode(Box::new(
+                    LetUnletNode{name, rhs, is_unlet: false}
+                )));
+            }
+        );
 
+        parse! (self;
+            name : self.name(),
+            "=:",
+            rhs : self.expression(),
+            ";",
+            {
+                return Some(StatementNode::LetUnletNode(Box::new(
+                    LetUnletNode{name, rhs, is_unlet: true}
+                )));
+            }
+        );
+        
+        /*
+        let pos = self.mark();
         if let Some(name) = self.name() {
         if self.expect_literal(":=") {
         if let Some(rhs) = self.expression() {
         if self.expect_literal(";") {
-            return Some(StatementNode::LetUnlet(Box::new(
+            return Some(StatementNode::LetUnletNode(Box::new(
                 LetUnletNode{name, rhs, is_unlet: false}
             )));
         }}}};
@@ -355,11 +509,12 @@ impl Parser {
         if self.expect_literal("=:") {
         if let Some(rhs) = self.expression() {
         if self.expect_literal(";") {
-            return Some(StatementNode::LetUnlet(Box::new(
+            return Some(StatementNode::LetUnletNode(Box::new(
                 LetUnletNode{name, rhs, is_unlet: true}
             )));
         }}}};
         self.reset(pos);
+        */
      
         None
     }
@@ -372,25 +527,25 @@ impl Parser {
         if let Some(token) = self.expect_type("NUMBER") {
             let value = Fraction::from_str(&token.string_[..]).unwrap();
             let value = FractionNode{value};
-            return Some(ExpressionNode::Fraction(Box::new(value)));
+            return Some(ExpressionNode::FractionNode(Box::new(value)));
         };
         
         if let Some(x) = self.array_literal() {
-            return Some(ExpressionNode::ArrayLiteral(Box::new(x)));
+            return Some(ExpressionNode::ArrayLiteralNode(Box::new(x)));
         }
         
         if let Some(lhs) = self.expression() {
         if let Some(op)  = self.binop() {
         if let Some(rhs) = self.expression() {
             return Some(
-                ExpressionNode::Binop(Box::new(
+                ExpressionNode::BinopNode(Box::new(
                     BinopNode{lhs, rhs, op}
             )));
         }}};
         self.reset(pos);
 
         if let Some(lookup) = self.lookup() {
-            return Some(ExpressionNode::Lookup(Box::new(lookup)));
+            return Some(ExpressionNode::LookupNode(Box::new(lookup)));
         };
 
         None
